cc69448c7cf709c7fca9afd1e3768c1a5072b23f
Richard E Lawrence <rlawrence@llogin1.launch>
2024-03-07 13:19:53 -0600

    Added shutdown button extension from https://github.com/EnsignMK/sd_shutdown_button

---
+++ extensions-builtin/sd_shutdown_button/README.md
@@ -0,0 +1,54 @@
+# sd_shutdown_button
+Easy way to exit stable diffusion web ui
+
+
+# What is this?
+This is an extension made for [AUTOMATIC1111's stable-diffusion-webui](https://github.com/AUTOMATIC1111/stable-diffusion-webui)
+
+# How does it work?
+- Click on the exit button 
+![example_image](stopbutton.png)
+afterwards this pop up appears on your tab
+![confirm_close](confirm_close.png)
+- .press OK if you want to exit the application ,  the stable diffusion tab will be automatically closed
+and the program will be stopped.
+
+- If you want to stay ,press the other button
+
+
+# credits 
+- I received some help from Automatic,showing me a function I needed for this extension.
+-  Also, I used some code from yuntian-deng  at https://huggingface.co/spaces/yuntian-deng/Gradio-Popup-Confirmation-Demo/tree/main
+
+# update
+12-08-23:  automatically closing the tab feature
+             - however it's important  do this:  
+                - whenever you start the webui and you see  the public url
+                ![image](https://github.com/EnsignMK/sd_shutdown_button/assets/91480302/2193381f-ae8b-4cd7-965d-be2e9bb29663)
+               
+              1. keep ctrl pressed and click on the link,now the new feature should be working.
+              
+              2. alternatively add the --autolaunch   argument before lauching stable diffusion then,
+              Stable webui automatically opens in the browser.
+              
+      
+
+
+        
+
+ ## potential issue: 
+ If the tab doesn't close no worries, it's a browser thing.Your webui is shutdown.
+                you will see this <img width="400" alt="image" src="https://github.com/EnsignMK/sd_shutdown_button/assets/91480302/2ad1d71a-f54b-48be-9025-82dee4b2d54a">
+
+
+               
+
+           
+            
+
+             
+             
+             
+
+
+
---
+++ extensions-builtin/sd_shutdown_button/scripts/shutdown.py
@@ -0,0 +1,78 @@
+from modules import script_callbacks
+import gradio as gr
+import os
+
+from modules.shared import opts
+
+js = """
+function close_window() {
+  if (confirm("Close the application?")) {
+
+    document.body.innerHTML = ''
+    var messageDiv = document.createElement('div');
+    messageDiv.textContent = 'You may close this window';
+    messageDiv.style.display = 'flex';
+    messageDiv.style.alignItems = 'center';
+    messageDiv.style.justifyContent = 'center';
+    messageDiv.style.textAlign = 'center';
+
+    messageDiv.style.fontWeight = 'bold';
+    messageDiv.style.fontSize = '40px'; // 
+
+    // Set the background color
+    document.body.style.backgroundColor = '#d49013'; 
+
+    // Append the message div to the body
+    document.body.appendChild(messageDiv);
+
+     setTimeout(function() {
+      window.close();
+    }, 2000);
+
+    return true;
+
+  } else {
+    return false;
+  }
+}
+"""
+
+
+def core():  # ui stuff
+    with gr.Row(elem_id="quicksettings", variant="compact"):
+        btn = gr.Button("Exit ⭕", elem_id="stop_button", size="sm", variant='stop')
+        hidden_checkbox = gr.Checkbox(visible=False)
+
+        def when(hidden_state):
+            if hidden_state:
+                os._exit(0)
+            return False
+    btn.click(when, hidden_checkbox, hidden_checkbox, _js=js)
+
+
+key = opts.data["quicksettings_list"][-1]
+
+
+def fun(key):  # figuring out where to place the stop button
+    after_this_compo = "setting_{}".format(key)
+    refresh_compo = "refresh_{}".format(key)
+    if key in opts.data_labels:
+        cm = opts.data_labels[key]
+        if cm.refresh is not None:
+            return refresh_compo
+        if cm.refresh is None:
+            return after_this_compo
+
+    else:
+        return after_this_compo
+
+
+def stop_button(component, **kwargs):
+    global key
+
+    if kwargs.get("elem_id") == fun(key):
+        core()
+
+
+script_callbacks.on_after_component(stop_button)
+
